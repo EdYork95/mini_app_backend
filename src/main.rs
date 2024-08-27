ba5d2e5fef20ac820259718750f78b96ca1562b2
EdYork <edyork95@gmail.com>
2024-08-27 17:00:06 +0100

    add a basic hello world route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("->> LISTENING on {:?}\n", listener.local_addr());
+    axum::serve(listener, routes::handler_hello().into_make_service())
+        .await
+        .unwrap();
 }
